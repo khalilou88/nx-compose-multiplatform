2b3a6655759503a2e4f05ab240db4b89efb94cf9
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-06-26 14:48:13 +0200

    Add nx support

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
     id("com.android.application").apply(false)
     id("com.android.library").apply(false)
     id("org.jetbrains.compose").apply(false)
+
+    id("io.github.khalilou88.jnxplus") version "0.2.0"
 }
